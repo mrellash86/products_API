10cf68b5154879917d685a04fb05e1b139f04f9c
unknown <mrellash86@gmail.com>
2022-04-01 13:23:38 -0500

    Modified : serilizer, wiews, settings, urls files of products_project

---
+++ products/serializers.py
@@ -0,0 +1,9 @@
+from socket import CAN_RAW
+from rest_framework import serializers
+from .models import Product
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description', 'inventory_quantity', 'price']  
+ 
--- products_project/settings.py
+++ products_project/settings.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+] 
 
 ROOT_URLCONF = 'products_project.urls'
 

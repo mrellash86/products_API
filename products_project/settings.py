ca7356b7f778ee03519543d27d88ebf3668f4ad6
unknown <mrellash86@gmail.com>
2022-04-01 09:29:20 -0500

    Changed database settings + Created Products app

--- products_project/settings.py
+++ products_project/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
- n     'django.contrib.messages',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
 
